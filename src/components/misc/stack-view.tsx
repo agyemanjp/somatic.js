894811b3175e6deb6e36f5e0b54b9166197edfb6
Diego Cisneros <diegocl02@gmail.com>
2020-09-28 17:38:06 -0300

    created a stack-view component

---
+++ src/components/misc/stack-view.tsx
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createElement } from '../../core'
+import { Component, Props, CSSProperties } from '../../types'
+import { mergeProps, colorLuminance } from '../../utils'
+import { StackPanel } from '../panels/stack-panel'
+import { HoverBox } from '../boxes/hover-box'
+
+export type Props = Props.Themed & Partial<Props.Panel> & {
+    /** Enable multiple or single items selection */
+    selectionMode?: "single" | "multiple"
+
+    /** List of values that will be rendered according to the template */
+    items: (string | number)[]
+
+    /** Component used to render each item */
+    template: Component<{ item: string | number }>
+
+    /** Selected item style */
+    selectedStyle: CSSProperties
+
+    /** Enable style based on linear gradient colors */
+    appearance?: "modern" | "minimalist"
+
+    /** Indexes of the selected items */
+    selectedItems: number[]
+}
+
+const defaultProps = {
+    selectionMode: "single" as const,
+    appearance: "minimalist" as const
+}
+
+type Messages = { type: "ITEM_SELECTED", data: { selectedIndexes: number[] } }
+
+export const StackView: Component<Props, Messages> = async (props) => {
+    const {
+        selectionMode,
+        items,
+        template,
+        selectedStyle,
+        appearance,
+        selectedItems,
+        theme,
+        postMsgAsync,
+        ...panelProps
+    } = mergeProps(defaultProps, props)
+
+    const colors = {
+        background: {
+            default: appearance === "minimalist"
+                ? colorLuminance(theme.colors.primary.light, 0.35)
+                : `linear-gradient(180deg, rgba(0,0,0,0.85}) 0%, rgba(0,0,0,0.4}) 100%)`,
+            selected: appearance === "minimalist"
+                ? colorLuminance(theme.colors.primary.dark, 0.5)
+                : `linear-gradient(180deg, ${theme.colors.secondary.dark} 0%, ${colorLuminance(theme.colors.secondary.light, 0.5)} 100%)`,
+            hover: appearance === "minimalist"
+                ? colorLuminance(theme.colors.primary.light, 0.30)
+                : `linear-gradient(180deg, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.2) 100%)`,
+            selectedHover: appearance === "minimalist"
+                ? colorLuminance(theme.colors.primary.dark, 0.35)
+                : `linear-gradient(180deg, ${colorLuminance(theme.colors.secondary.dark, 0.2)} 0%, ${colorLuminance(theme.colors.secondary.light, 0.9)} 100%)`
+        },
+        foreground: {
+            default: appearance === "minimalist" ? theme.colors.blackish : "white",
+            selected: "white",
+            hover: appearance === "minimalist" ? "black" : "white"
+        }
+    }
+
+    return <StackPanel {...panelProps}>
+        {
+            items.map((item, index) => {
+                const isSelected = selectedItems.includes(index)
+                return <HoverBox
+                    theme={theme}
+                    style={{
+                        backgroundColor: isSelected
+                            ? colors.background.selected
+                            : colors.background.default,
+                        color: isSelected
+                            ? colors.foreground.selected
+                            : colors.foreground.default,
+                        ...selectedStyle
+                    }}
+                    hoverStyle={{
+                        color: isSelected ? colors.foreground.selected : colors.foreground.default,
+                        background: isSelected ? colors.background.selectedHover : colors.background.hover,
+                    }}
+                    onClick={() => {
+                        if (postMsgAsync) {
+                            const selectedIndexes = selectionMode === "single"
+                                ? [index]
+                                : [...selectedItems, index]
+                            postMsgAsync({
+                                type: "ITEM_SELECTED",
+                                data: {
+                                    selectedIndexes
+                                }
+                            })
+                        }
+                    }}>
+                    {template({ item })}
+                </HoverBox>
+            })
+        }
+    </StackPanel>
+}
--- test/components.test.tsx
+++ test/components.test.tsx
@@ -1,121 +1,142 @@
-// /* eslint-disable @typescript-eslint/no-var-requires */
-// /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // /* eslint-disable fp/no-mutation */
 // /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 // /* eslint-disable init-declarations */
 
-// import * as assert from "assert"
-// import { Component, createElement, render } from '../dist/index.js'
-// import { Orientation, Alignment, CSSProperties } from '../dist/types.js'
-// import { StackPanel, Alert } from '../dist/components/index.js'
-// import { describe, it, beforeEach, afterEach } from 'mocha'
-// const jsdom = require('mocha-jsdom')
-// jsdom({ url: 'http://localhost', skipWindowCheck: true })
+import * as assert from "assert"
+import { Component, createElement, render } from '../dist/index.js'
+import { Orientation, Alignment, CSSProperties } from '../dist/types.js'
+import { StackPanel, Alert, StackView } from '../dist/components/index.js'
+import { describe, it, beforeEach, afterEach } from 'mocha'
+const jsdom = require('mocha-jsdom')
+jsdom({ url: 'http://localhost', skipWindowCheck: true })
 
-// const theme = {
-// 	colors: {
-// 		primary: {
-// 			light: "#86a1bc",
-// 			dark: "#34495e"
-// 		},
-// 		secondary: {
-// 			light: "#d477b0",
-// 			dark: "#C64B97"
-// 		},
+const theme = {
+    colors: {
+        primary: {
+            light: "#86a1bc",
+            dark: "#34495e"
+        },
+        secondary: {
+            light: "#d477b0",
+            dark: "#C64B97"
+        },
 
-// 		error: "red",
-// 		warning: "yellow",
-// 		info: "green",
+        error: "red",
+        warning: "yellow",
+        info: "green",
 
-// 		whitish: "whitesmoke",
-// 		blackish: "#555555",
-// 		grayish: "#808080"
-// 	},
-// 	fonts: {
-// 		text: "normal normal 12px serif",
-// 		textAlt: "italics normal 12px serif",
-// 		link: "normal normal 12px sans-serif",
-// 		titleBig: "normal bold 24px sans-serif",
-// 		titleMedium: "normal normal 20px sans-serif",
-// 		titleSmall: "normal bold 12px sans-serif",
-// 		tiny: "normal normal 10 serif"
-// 	},
-// 	thickness: 1
-// }
+        whitish: "whitesmoke",
+        blackish: "#555555",
+        grayish: "#808080"
+    },
+    fonts: {
+        text: "normal normal 12px serif",
+        textAlt: "italics normal 12px serif",
+        link: "normal normal 12px sans-serif",
+        titleBig: "normal bold 24px sans-serif",
+        titleMedium: "normal normal 20px sans-serif",
+        titleSmall: "normal bold 12px sans-serif",
+        tiny: "normal normal 10 serif"
+    },
+    thickness: 1
+}
 
-// describe("Components", () => {
+describe("Components", () => {
 
-// 	// eslint-disable-next-line fp/no-let
-// 	let container: HTMLDivElement | null
+    // eslint-disable-next-line fp/no-let, init-declarations
+    let container: HTMLDivElement | null
 
-// 	beforeEach(() => {
-// 		container = document.createElement('div')
-// 		document.body.appendChild(container)
-// 	})
+    beforeEach(() => {
+        // eslint-disable-next-line fp/no-mutation
+        container = document.createElement('div')
+        document.body.appendChild(container)
+    })
 
-// 	afterEach(() => {
-// 		if (container) container?.remove()
-// 		container = null
-// 	})
+    afterEach(() => {
+        if (container) container?.remove()
+        // eslint-disable-next-line fp/no-mutation
+        container = null
+    })
 
-// 	describe("Stack-panel", () => {
-// 		it("should render the element and its children with the default props", async () => {
-// 			const node = <StackPanel>{`test`}</StackPanel>
-// 			const renderedNode = await render(node)
+    describe("StackView", () => {
+        it("should render the element and its children with the default props", async () => {
+            const node = <StackView
+                selectionMode="single"
+                selectedItems={[0]}
+                selectedStyle={{}}
+                template={(props) => <span>{props.item}</span>}
+                items={['test0', 'test1', 'test2']}
+                theme={theme} />
+            const renderedNode = await render(node)
 
-// 			// Attach the element to the dom container
-// 			container?.appendChild(renderedNode)
+            // Attach the element to the dom container
+            container?.appendChild(renderedNode)
 
-// 			assert.equal(container?.children[0].textContent, 'test')
-// 		})
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            assert.equal(container!.children[0].children[0].children[1].textContent, 'test0')
+        })
+    })
 
-// 		it("should set flex direction attribute as column when passing orientation props", async () => {
-// 			const node = <StackPanel orientation={Orientation.vertical}>{`test`}</StackPanel>
-// 			const renderedNode = await render(node)
+    // 	describe("Stack-panel", () => {
+    // 		it("should render the element and its children with the default props", async () => {
+    // 			const node = <StackPanel>{`test`}</StackPanel>
+    // 			const renderedNode = await render(node)
 
-// 			// Attach the element to the dom container
-// 			container?.appendChild(renderedNode)
+    // 			// Attach the element to the dom container
+    // 			container?.appendChild(renderedNode)
 
-// 			const elt = container?.children[0]
-// 			if (elt && 'style' in elt)
-// 				assert.equal(elt["style"]["flex-direction"], 'column')
+    // 			assert.equal(container?.children[0].textContent, 'test')
+    // 		})
 
-// 		})
+    // 		it("should set flex direction attribute as column when passing orientation props", async () => {
+    // 			const node = <StackPanel orientation={Orientation.vertical}>{`test`}</StackPanel>
+    // 			const renderedNode = await render(node)
 
-// 		it("should set justify content attribute as centered when passing items align props", async () => {
-// 			const node = <StackPanel itemsAlignH={Alignment.center}>{`test`}</StackPanel>
-// 			const renderedNode = await render(node)
+    // 			// Attach the element to the dom container
+    // 			container?.appendChild(renderedNode)
 
-// 			// Attach the element to the dom container
-// 			container?.appendChild(renderedNode)
+    // 			const elt = container?.children[0]
+    // 			if (elt && 'style' in elt)
+    // 				assert.equal(elt["style"]["flex-direction"], 'column')
 
-// 			const elt = container?.children[0]
-// 			if (elt && 'style' in elt)
-// 				assert.equal(elt["style"]["justify-content"], 'center')
+    // 		})
 
-// 		})
-// 	})
+    // 		it("should set justify content attribute as centered when passing items align props", async () => {
+    // 			const node = <StackPanel itemsAlignH={Alignment.center}>{`test`}</StackPanel>
+    // 			const renderedNode = await render(node)
 
-// 	describe("Alert", () => {
-// 		it("should render the element and its content", async () => {
-// 			const node = <Alert type={"error"} theme={theme} content={'test-msg'}></Alert>
-// 			const renderedNode = await render(node)
+    // 			// Attach the element to the dom container
+    // 			container?.appendChild(renderedNode)
 
-// 			// Attach the element to the dom container
-// 			container?.appendChild(renderedNode)
+    // 			const elt = container?.children[0]
+    // 			if (elt && 'style' in elt)
+    // 				assert.equal(elt["style"]["justify-content"], 'center')
 
-// 			assert.equal(container?.children[0].textContent, ' Error! test-msg')
-// 		})
-// 		it("should render the element with its corresponding style according to its type", async () => {
-// 			const node = <Alert type={"error"} theme={theme} content={'test-msg'}></Alert>
-// 			const renderedNode = await render(node)
+    // 		})
+    // 	})
 
-// 			// Attach the element to the dom container
-// 			container?.appendChild(renderedNode)
+    // 	describe("Alert", () => {
+    // 		it("should render the element and its content", async () => {
+    // 			const node = <Alert type={"error"} theme={theme} content={'test-msg'}></Alert>
+    // 			const renderedNode = await render(node)
 
-// 			const elt = container?.children[0].children[0]
-// 			if (elt && 'style' in elt)
-// 				assert.equal(elt["style"]["background-color"], theme.colors.error)
-// 		})
-// 	})
-// })
+    // 			// Attach the element to the dom container
+    // 			container?.appendChild(renderedNode)
+
+    // 			assert.equal(container?.children[0].textContent, ' Error! test-msg')
+    // 		})
+    // 		it("should render the element with its corresponding style according to its type", async () => {
+    // 			const node = <Alert type={"error"} theme={theme} content={'test-msg'}></Alert>
+    // 			const renderedNode = await render(node)
+
+    // 			// Attach the element to the dom container
+    // 			container?.appendChild(renderedNode)
+
+    // 			const elt = container?.children[0].children[0]
+    // 			if (elt && 'style' in elt)
+    // 				assert.equal(elt["style"]["background-color"], theme.colors.error)
+    // 		})
+    // 	})
+})
