b62fec86526b8b0d69eceb4723137e77e6be9da4
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-08-09 16:47:48 +0000

    refactor preview to component

--- src/preview.tsx
+++ src/preview.tsx
@@ -1,85 +1,98 @@
-
-import { CarouselPanel } from "./components/panels/carousel-panel"
-import { createElement, mountElement } from "./core"
+import { CarouselPanel } from './components/panels/carousel-panel'
+import { createElement, mountElement } from './core'
 import * as Icons from './icons'
 
+function Preview() {
+	const handleClick = (tab: string) => {
+		const iconsContainer = document.getElementById('iconsContainer')
+		const componentsContainer = document.getElementById('componentsContainer')
+
+		if (iconsContainer && componentsContainer) {
+			if (tab === 'icons') {
+				iconsContainer.style.display = 'grid'
+				componentsContainer.style.display = 'none'
+			}
+			else if (tab === 'components') {
+				iconsContainer.style.display = 'none'
+				componentsContainer.style.display = 'block'
+			}
+		}
+	}
+
+	const renderIcons = () => (
+		<div
+			id="iconsContainer"
+			style={{
+				display: 'grid',
+				gridTemplateColumns: '25% 25% 25% 25%',
+				gridGap: '10px',
+				padding: '20px',
+				justifyContent: 'space-evenly',
+				alignContent: 'space-evenly',
+			}}
+		>
+			{Object.entries(Icons).map(([key, ico]) => (
+				<div
+					id={key}
+					style={{
+						alignContent: 'center',
+						border: '1px solid',
+						justifyContent: 'center',
+						textAlign: 'center',
+						paddingTop: '10px',
+						paddingBottom: '50px',
+						height: '60px',
+					}}
+				>
+					{createElement(ico, {})}
+					<p>{key}</p>
+				</div>
+			))}
+		</div>
+	)
+
+	const renderComponents = () => (
+		<div id="componentsContainer" style={{ display: 'none' }}>
+			<p>Carousel Panel</p>
+			<div style={{ height: '100px' }}>
+				<CarouselPanel chevronSize="50px" id="carousel" style={{ height: '300px', width: '300px' }}>
+					<div>
+						<img src="https://craftinginterpreters.com/image/chunks-of-bytecode/ast.png" alt="AST" />
+					</div>
+					<div>
+						<img src="https://www.digitalocean.com/_next/static/media/default-avatar.14b0d31d.jpeg" alt="Avatar" />
+					</div>
+					<div>Some sample text</div>
+					<div>
+						<button>A button</button>
+					</div>
+				</CarouselPanel>
+			</div>
+		</div>
+	)
+
+	return (
+		<div>
+			<div style={{ display: 'flex', height: '20%' }}>
+				<p id="iconLabel" onClick={() => handleClick('icons')}>Icons</p>
+				<p id="componentLabel" style={{ paddingLeft: '10px' }} onClick={() => handleClick('components')}>
+					Components
+				</p>
+			</div>
+			{renderIcons()}
+			{renderComponents()}
+		</div>
+	)
+}
 
 (async () => {
-
-	if (typeof document !== "undefined") {
-		document.addEventListener("DOMContentLoaded", async event => {
-			const rootContainerNode = document.getElementById("root")
-			if (!rootContainerNode) { throw new Error(`Root container with id ${rootContainerNode} not found`) }
-
-			const IconPreview = (() => <div style={{
-
-			}}>
-				<div style={{ display: "flex", height: "20%" }}>
-					<p id="iconLabel">Icons</p>
-					<p id="componentLabel" style={{paddingLeft: "10px"}}>Components</p>
-				</div>
-				{/* Icons Container */}
-				<div
-					id="iconsContainer" style={{
-						display: "grid",
-						height: "100%",
-						gridTemplateColumns: "25% 25% 25% 25%",
-
-						gridGap: "10px",
-						padding: "20px",
-						justifyContent: "space-evenly",
-						alignContent: "space-evenly"
-					}}>
-					{Object.entries(Icons).map(([key, ico]) => (<div
-						id={key}
-						style={{
-							alignContent: "center",
-							border: "1px solid",
-							justifyContent: "center",
-							textAlign: "center",
-							paddingTop: "10px",
-							paddingBottom: "50px",
-							height: "60px"
-						}}>{createElement(ico, {})}
-						<p>{key}</p>
-					</div>))}
-				</div>
-				{/** Components Container */}
-				<div id="componentsContainer" style={{ display: 'none' }}>
-					<p>Carousel Panel</p>
-					<div style={{ height: '100px' }}>
-						<CarouselPanel chevronSize='50px' id='carousel' style={{ height: "300px", width: "300px" }}>
-							<div><img src="https://craftinginterpreters.com/image/chunks-of-bytecode/ast.png" /></div>
-							<div><img src="https://www.digitalocean.com/_next/static/media/default-avatar.14b0d31d.jpeg" /></div>
-							<div>Some sample text</div>
-							<div><button>A button</button></div>
-						</CarouselPanel>
-					</div>
-				</div>
-
-
-			</div>)()
-
-			await mountElement(IconPreview, rootContainerNode as Element)
-
-			const iconLabel = document.getElementById('iconLabel')
-			const componentLabel = document.getElementById('componentLabel')
-			const iconsContainer = document.getElementById('iconsContainer')
-			const componentsContainer = document.getElementById('componentsContainer')
-
-			if (iconLabel && componentLabel && iconsContainer && componentsContainer) {
-
-				// Click listeners for 'Icons' and 'Components'
-				iconLabel.addEventListener('click', function () {
-					iconsContainer.style.display = 'grid'
-					componentsContainer.style.display = 'none'
-				})
-
-				componentLabel.addEventListener('click', function () {
-					iconsContainer.style.display = 'none'
-					componentsContainer.style.display = 'block'
-				})
+	if (typeof document !== 'undefined') {
+		document.addEventListener('DOMContentLoaded', async event => {
+			const rootContainerNode = document.getElementById('root')
+			if (!rootContainerNode) {
+				throw new Error(`Root container with id root not found`)
 			}
+			await mountElement(<Preview/>, rootContainerNode)
 		})
 	}
 })()
